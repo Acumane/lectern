6bd0ade29ce797acc29d8f8c61ef8205302fc403
Bren <94324138+Acumane@users.noreply.github.com>
2023-06-10 21:56:59 -0400

    Pages, read + text in sync*

--- build.py
+++ build.py
@@ -1,34 +1,53 @@
+import pdftotext
 import asyncio
 import edge_tts
-
-TEXT = """
-This was nothing but a formality, instituted to reassure the masses, for the initiates
-knew well enough that during his fast the artist would never in any circumstances, not
-even under forcible compulsion, swallow the smallest morsel of food; the honor of his
-profession forbade it.
-"""
+from os.path import isfile
+from threading import Thread
+from read import play, display
 
 VOICE = "en-GB-SoniaNeural"
-OUTPUT_FILE = "test.mp3"
-WEBVTT_FILE = "test.vtt"
 
-
-async def amain() -> None:
-	communicate = edge_tts.Communicate(TEXT, VOICE, rate="+60%")
+async def synth(text, n) -> None:
+	OUTPUT = f"page-{n}.mp3"
+	SUBS = f"page-{n}.vtt"
 	submaker = edge_tts.SubMaker()
-	with open(OUTPUT_FILE, "wb") as file:
+	communicate = edge_tts.Communicate(text, VOICE, rate="+60%")
+	print(f"   #{n}...", end=' ', flush=True)
+	with open(OUTPUT, "wb") as file:
 		async for chunk in communicate.stream():
 			if chunk["type"] == "audio":
 				file.write(chunk["data"])
 			elif chunk["type"] == "WordBoundary":
 				submaker.create_sub((chunk["offset"], chunk["duration"]), chunk["text"])
 
-	with open(WEBVTT_FILE, "w", encoding="utf-8") as file:
+	with open(SUBS, "w", encoding="utf-8") as file:
 		file.write(submaker.generate_subs(1))
+	print("\N{check mark}")
 
+with open("cosmos.pdf", "rb") as pdf:
+	pages = pdftotext.PDF(pdf)
+
+pRange = input(f"Pick reading range (1-{len(pages)}): ")
+first, last  = [int(x) for x in pRange.split('-')]
+
+#change
 
 loop = asyncio.get_event_loop_policy().get_event_loop()
 try:
-	loop.run_until_complete(amain())
+	print("Synthesizing:")
+	for pg in range(first, last+1):
+		if isfile(f"page-{pg}.mp3"):
+			print(f"   #{pg}    *")
+			continue
+		loop.run_until_complete(synth(pages[pg-1], pg))
+	for pg in range(first, last+1):
+		print(f"\n\n{pg}", "—"*60, "\n")
+		threads = []
+		threads.append(Thread(target=play, args=(pg,) ))
+		threads.append(Thread(target=asyncio.run, args=(display(pg), ) ))
+		
+		for t in threads: t.start() # Start both threads
+		for t in threads: t.join()  # Wait for them (page) to finish
+
 finally:
 	loop.close()
--- read.py
+++ read.py
@@ -1,28 +1,27 @@
 import asyncio
 from threading import Thread
-from playsound import playsound as play
+from playsound import playsound
 
 # Function to convert timestamp into seconds
 def convertTime(timestamp):
 	hours, minutes, seconds = map(float, timestamp.split(':'))
 	return hours * 3600 + minutes * 60 + seconds
 
-async def display(delay, caption):
+async def printer(delay, caption):
 	await asyncio.sleep(delay)
 	print(caption, end=' ', flush=True)
 
-async def main():
-	with open('test.vtt', 'r') as f:
+def play(n):
+	playsound(f'page-{n}.mp3')
+
+async def display(n):
+	with open(f'page-{n}.vtt', 'r') as f:
 		next(f); next(f) # Skip first 2 lines
 		tasks = []
 		for line in f:
-			time, _ = line.split(' --> ')
+			_, time = line.split(' --> ')
 			start = convertTime(time)
 			caption = next(f).strip(); next(f)
-			tasks.append(display(start, caption))
+			tasks.append(printer(start, caption))
 
 		await asyncio.gather(*tasks)
-
-# play('test.mp3')
-Thread(target=play, args=('test.mp3',) ).start()
-asyncio.run(main())
